c302629d4bb6c85938ea5d862d9e66cfaa857945
Darren <darren@bruning.net.nz>
2019-07-20 14:41:53 +1200

    Build script

---
+++ Build for Raspberry Pi.ps1
@@ -0,0 +1,12 @@
+# Setup path 
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath
+Push-Location $scriptDir
+[Environment]::CurrentDirectory = $PWD
+"Current directory at start of build.ps1 is: $PWD"
+
+# Build (from app directory)
+cd ClockWeatherDisplay.App
+
+dotnet build --runtime win10-arm --configuration release --output out 
+
